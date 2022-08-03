a6784bdefbc4dbe7aafe68a6895775f3a18e41c4
Koloru <sos.llyr@gmail.com>
2022-08-03 15:35:32 +0800

    add css reset and install google webfonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+//npm imports
+import WebFont from 'webfontloader';
+
+import { useEffect } from 'react';
+
+// styles
 import './App.sass';
 
 function App() {
+
+  // import google font
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'sans-serif']
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App container">
       <p className='test'>Hello</p>
     </div>
   );
